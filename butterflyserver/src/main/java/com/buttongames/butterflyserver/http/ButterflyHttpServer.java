fa82a961cb0bf7e56b41e96074ec837dc677649b
topet2k12001 <topet2k12001@users.noreply.github.com>
2020-06-21 23:23:54 +0800

    Reverting to previous release setting of "true" to reverse the logic of PCB ID check.

--- butterflyserver/src/main/java/com/buttongames/butterflyserver/http/ButterflyHttpServer.java
+++ butterflyserver/src/main/java/com/buttongames/butterflyserver/http/ButterflyHttpServer.java
@@ -212,7 +212,7 @@ public class ButterflyHttpServer {
             final LocalDateTime now = LocalDateTime.now();
             final ButterflyUser newUser = new ButterflyUser("0000", now, now, 10000);
             userDao.create(newUser);
-
+            //previous release value is "true;" value of "false" causes "Network Error" and forces game into "Maintenance Mode"
             machine = new Machine(newUser, requestBodyPcbId, LocalDateTime.now(), false, 0);
             machineDao.create(machine);
 
